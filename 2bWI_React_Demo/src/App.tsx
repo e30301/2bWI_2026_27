454c033ffbd20b79c7220a50fada5f191c35298c
isaak vogel <isaak.vogel@student.htldornbirn.at>
2026-09-15 09:12:03 +0200

    init

---
+++ 2bWI_React_Demo/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Card from "./components/Card";
+export default function App() {
+
+  return (
+    <>
+      <div>hello</div>
+        <Card /></>
+  )
+}
+
